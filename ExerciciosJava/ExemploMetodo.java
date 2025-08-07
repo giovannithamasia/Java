65198fa43c866191975ee6ce13b9a6cafdec14c3
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2025-08-07 15:05:06 -0300

    Organizando

---
+++ ExerciciosJava/ExemploMetodo.java
@@ -0,0 +1,13 @@
+
+public class ExemploMetodo {
+    public static void main(String[] args) {
+
+        ImprimirMensagem();
+
+    }
+
+    public static void ImprimirMensagem() {
+        System.out.println("Oi turma");
+    }
+
+}
